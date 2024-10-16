961a955d3224a907e478a3c2a1df26c704e87d9a
Arthur730 <134970703+Arthur730@users.noreply.github.com>
2024-10-16 19:48:23 +0300

    ji

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public void ChangeSceneByName(string sceneName)
+    {
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning($"Сцена с именем {sceneName} не найдена.");
+        }
+    }
+
+    public void ChangeSceneByIndex(int sceneIndex)
+    {
+        if (sceneIndex >= 0 && sceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning($"Сцена с индексом {sceneIndex} не найдена.");
+        }
+    }
+}
